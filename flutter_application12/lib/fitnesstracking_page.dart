4533fdf67086d7cbc22464e0bdd38c7f3093da51
Nipuna Fernando <148954130+andfernando-code@users.noreply.github.com>
2024-03-14 18:17:16 +0530

    Update fitnesstracking_page.dart

--- flutter_application12/lib/fitnesstracking_page.dart
+++ flutter_application12/lib/fitnesstracking_page.dart
@@ -1,4 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application12/calendar_page.dart';
+import 'package:flutter_application12/healthreminders_page.dart';
+import 'package:flutter_application12/settings_page.dart';
 
 class FitnessTrackingPage extends StatelessWidget {
   @override
@@ -128,6 +132,38 @@ class FitnessTrackingPage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        // Replace BottomAppBar with CurvedNavigationBar
+        backgroundColor:
+            Color.fromARGB(255, 207, 190, 210), // Change bottom app bar color
+        color: Colors.blue, // Change the background color of the bar items
+        buttonBackgroundColor: const Color.fromARGB(
+            251, 157, 172, 185), // Change the background color of the buttons
+        height: 60, // Adjust the height of the bar
+        items: const <Widget>[
+          Icon(Icons.notifications, size: 30),
+          Icon(Icons.event, size: 30),
+          Icon(Icons.settings, size: 30),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HealthRemindersPage()),
+            ); // Navigate to Notifications Page
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CalendarPage()),
+            ); // Navigate to Calender Page
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SettingsPage()),
+            ); // Navigate to Settings Page
+          }
+        },
       ),
     );
   }
