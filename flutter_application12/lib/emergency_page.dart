eee419a07acc6c1753519618399c5dfa58fb0bf8
Nipuna Fernando <148954130+andfernando-code@users.noreply.github.com>
2024-03-14 18:16:03 +0530

    Update emergency_page.dart

--- flutter_application12/lib/emergency_page.dart
+++ flutter_application12/lib/emergency_page.dart
@@ -1,55 +1,140 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_application12/calendar_page.dart';
+import 'package:flutter_application12/healthreminders_page.dart';
+import 'package:flutter_application12/home_page.dart';
+import 'package:flutter_application12/settings_page.dart';
 
-class EmergencyPage extends StatelessWidget {
+class emergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Emergency'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: 200, // Adjust the height of the cover image
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/ambu.jpg'),
-                fit: BoxFit.cover,
-              ),
+        titleTextStyle: const TextStyle(
+          fontFamily: 'FontMain',
+          color: Colors.black,
+          fontSize: 38,
+          fontWeight: FontWeight.bold,
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(
+            255, 255, 255, 255), // Change app bar background color
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 30,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'In case of emergency,',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
                 ),
-                SizedBox(height: 10),
-                Text(
-                  'Please stay calm and call an ambulance immediately.',
-                  style: TextStyle(
-                    fontSize: 16,
+              );
+            },
+            );
+          },
+        ),
+        
+      ),
+      
+      body: Container(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                // "1.jpg" image outside the common padding
+                Image(
+                  image: AssetImage("assets/images/1.jpeg"),
+                ),
+                SizedBox(height: 20),
+
+                // Common vertical padding for the following elements
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "In Case of Emergency",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                          "Please stay calm and call an ambulance immediately."),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Implement emergency functionality
+                          print('emergency call button pressed');
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(0.0),
+                            backgroundColor: Colors.red,
+                            fixedSize: Size(250, 100),
+                            elevation: 15,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Emergency Call",
+                              style:
+                                  TextStyle(fontSize: 25, color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.call,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              ();
-            },
-            child: const Text('Call Ambulance'),
-          ),
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        // Replace BottomAppBar with CurvedNavigationBar
+        backgroundColor: Colors.white, // Change bottom app bar color
+        color: Colors.blue, // Change the background color of the bar items
+        buttonBackgroundColor: const Color.fromARGB(
+            251, 157, 172, 185), // Change the background color of the buttons
+        height: 60, // Adjust the height of the bar
+        items: const <Widget>[
+          Icon(Icons.notifications, size: 30),
+          Icon(Icons.event, size: 30),
+          Icon(Icons.settings, size: 30),
         ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HealthRemindersPage()),
+            ); // Navigate to Notifications Page
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CalendarPage()),
+            ); // Navigate to Calender Page
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SettingsPage()),
+            ); // Navigate to Settings Page
+          }
+        },
       ),
     );
   }
